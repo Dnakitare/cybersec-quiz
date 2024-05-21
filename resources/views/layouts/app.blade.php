7dc84ca30d6737cb2170aa1715e79d8c9020cf71
Daniel Nakitare <dnakitare@gmail.com>
2024-05-21 11:16:50 -0600

    feat: Add relationships between Category, Quiz, and Question models
    The code changes include adding relationships between the Category, Quiz, and Question models. The Category model now has a hasMany relationship with the Quiz model, and the Quiz model has a hasMany relationship with the Question model. These relationships are necessary for implementing the core functionality of the quiz application.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Cybersecurity Quiz Platform</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -13,9 +13,15 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        @yeild('contents')
+
+        @livewireScripts
+        @vite('resources/js/app.js')
+        
+        {{-- <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             <livewire:layout.navigation />
 
             <!-- Page Heading -->
@@ -31,6 +37,6 @@
             <main>
                 {{ $slot }}
             </main>
-        </div>
+        </div> --}}
     </body>
 </html>
