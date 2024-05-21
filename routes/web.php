c16e87559120c1eac41b1042e4657999efc83006
Daniel Nakitare <dnakitare@gmail.com>
2024-05-21 15:11:36 -0600

    feat: Enable user dashboard route and remove commented code
    The code changes in this commit enable the user dashboard route by uncommenting the relevant code in the `web.php` file. The commented code for the dashboard route has been removed.
    
    This change allows authenticated users to access their dashboard and view the `UserDashboardComponent` Livewire component.

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-// Route::view('dashboard', 'dashboard')
-//     ->middleware(['auth', 'verified'])
-//     ->name('dashboard');
-
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', UserDashboardComponent::class)->name('dashboard');
 });
